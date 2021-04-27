7202f1f80aecc9c53a838554be945d3f37825763
Roman Rabodzei <roman.rabodzei@outlook.com>
2021-04-27 22:14:01 +0300

    Update pipeline file

--- services/key_vault.tf
+++ services/key_vault.tf
@@ -6,12 +6,12 @@ resource "azurerm_key_vault" "key_vault" {
   name                            = lower("${var.aks_cluster_name}keyvault${var.environment}")
   resource_group_name             = var.resource_group_name
   location                        = var.location
-  tenant_id                       = data.azurerm_client_config.current.tenant_id
+  tenant_id                       = var.tenant_id
   sku_name                        = "premium"
   enabled_for_template_deployment = true
   enable_rbac_authorization       = false
   soft_delete_retention_days      = 14
-  access_policy = [{
+  /*access_policy = [{
     application_id = var.client_id
     object_id      = var.client_id
     tenant_id      = var.tenant_id
@@ -71,7 +71,7 @@ resource "azurerm_key_vault" "key_vault" {
       "setsas",
       "update"
     ]
-  }]
+  }]*/
   network_acls {
     default_action = "Deny"
     bypass         = "AzureServices"
@@ -90,11 +90,25 @@ resource "azurerm_key_vault" "key_vault" {
 }
 
 # AKV secrets and access policies
+
+resource "azurerm_role_assignment" "key_vault" {
+  scope                = azurerm_key_vault.key_vault.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = var.object_id
+}
+
 resource "azurerm_key_vault_secret" "mysql_secret" {
   name         = "mysqlsecret"
   value        = random_string.mysql_admin_password.result
   key_vault_id = azurerm_key_vault.key_vault.id
 }
+
+resource "azurerm_role_assignment" "key_vault" {
+  scope                = azurerm_key_vault.key_vault.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_mysql_server.mysql_server.identity[0].principal_id
+}
+/*
 resource "azurerm_key_vault_access_policy" "mssql_access_policy" {
   key_vault_id = azurerm_key_vault.key_vault.id
   tenant_id    = azurerm_mysql_server.mysql_server.identity[0].tenant_id
@@ -104,4 +118,4 @@ resource "azurerm_key_vault_access_policy" "mssql_access_policy" {
     "Get",
     "List"
   ]
-}
+}*/
