29dc42ba14d7a308bf1c64ddfcf03cb36a427cdb
Roman Rabodzei <roman.rabodzei@outlook.com>
2021-04-27 22:33:49 +0300

    Update pipeline file

--- services/key_vault.tf
+++ services/key_vault.tf
@@ -92,7 +92,7 @@ resource "azurerm_key_vault" "key_vault" {
 # AKV secrets and access policies
 resource "azurerm_key_vault_access_policy" "devops_access_policy" {
   key_vault_id = azurerm_key_vault.key_vault.id
-  object_id    = var.client_id
+  object_id    = var.object_id
   tenant_id    = var.tenant_id
 
   secret_permissions = [
